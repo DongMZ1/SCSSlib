f9669e5b1b97f0bb0fd3be5d58ce28b7caab1acf
DongMZ1 <915094594@qq.com>
2021-05-18 22:39:43 -0400

    add button, navitem, cards for my scss lib

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import './SCSS/button.scss'
+import './SCSS/navitem.scss'
+import './SCSS/card.scss'
+import image1 from './1.jpg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <br />
+      <div className="body">
+      <div className="link_swap">hover me</div>
+      <div onClick={()=>{}} className="link_rotate">hover me</div>
+      <div onClick={()=>{}} className="link_creative_1" >hover me</div>
+      <div onClick={()=>{}} className="link_button_stretch" >hover me</div>
+      <div onClick={()=>{}} className="link_glowing" >hover me</div>
+      <div onClick={()=>{}} className="link_twolayerswap" >hover me</div>
+      <div onClick={()=>{}} className="link_scale" >hover me</div>
+
+      <div onClick={()=>{}} className="link_circularswapping" >hover me</div>
+      <div onClick={()=>{}} className="link_swaplefttoright" >hover me</div>
+      <div onClick={()=>{}} className="link_bordergrow" >hover me</div>
+      <div onClick={()=>{}} className="link_firstletterrotate" ><span>H</span>over me</div>
+      <div onClick={()=>{}} className="link_creativeswap1" >hover me</div>
+      <div onClick={()=>{}} className="navitem_grow_border" >hover me</div>
+      <div onClick={()=>{}} className="navitem_backgroundborder">hover me</div>
+      <div onClick={()=>{}} className="navitem_underline" >hover me</div>
+      <div onClick={()=>{}} className="navitem_rotate" >hover me</div>
+      <div onClick={()=>{}} className="navitem_twoborder_background" >hover me</div>
+      </div>	
+      </>
   );
 }
 
